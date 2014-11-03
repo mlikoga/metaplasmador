75ca869cdd961eaf44c87e82172da107bdb06486
Marcelo Li Koga <marcelo.koga@polilibras.com.br>
2014-11-02 23:31:22 -0200

    vv

--- js/regra.js
+++ js/regra.js
@@ -5,12 +5,16 @@ function Regra(origem, destino) {
 }
 
 Regra.EMPTY_CHAR = '#';
+Regra.CJ_START = '{';
+Regra.CJ_END   = '}';
 
 Regra.prototype.aplicar = function (input) {
     var patternPrefix = '';
     var patternCore = '';
     var patternSuffix = '';
     var origemAux = this.origem;
+
+    // Simbolo #
     if (origemAux[0] == Regra.EMPTY_CHAR) {
         origemAux = origemAux.substr(1);
         patternPrefix = '^' + patternPrefix;
@@ -19,6 +23,9 @@ Regra.prototype.aplicar = function (input) {
         origemAux = origemAux.substr(0, origemAux.length - 1);
         patternSuffix = patternSuffix + '$';
     }
+
+    // Conjuntos
+    
 
     // O que sobrou da origem é o core
     patternCore = origemAux;
